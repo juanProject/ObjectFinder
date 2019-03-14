314773bb581652f6e2a97bcfdc9d4b52005e916f
CharafZellou <charaf.zellou@gmail.com>
2019-03-14 20:46:02 +0100

    Added file readed

--- src/main/scala/ObjectFinder.scala
+++ src/main/scala/ObjectFinder.scala
@@ -1,7 +1,21 @@
+def main(args: Array[String]): Unit = {
+  val lines = linesFromFile("/Users/vincent.vanhassel/ESGI-cours/4-IBC/scala/IdeaProjects/scala-spark-boilerplate/src/main/scala/com/fakir/samples/fileExample.csv")
+  val withoutHeader = getRidOfHeader(lines)
+  filterLinesFile(withoutHeader).foreach(println)
+}
 
+def linesFromFile(path: String): Array[String] = {
+  import scala.io._
+  Source.fromFile(path).getLines.toArray
+}
 
-object ObjectFinder {
-  def main(args: Array[String]): Unit = {
-    println("Hello World")
-  }
+def getRidOfHeader (array: Array[String]) : Array[String] = {
+  val header = array.head
+  array.filter(line => line != header)
+}
+
+def filterLinesFile(array: Array[String]) : Array[String] = {
+  val dataFiltered = array.filter(line => line.split(", ")(2).toInt > 2000)
+  val result = dataFiltered.filter(line => line.split(", ")(3).toInt > 1)
+  result
 }
