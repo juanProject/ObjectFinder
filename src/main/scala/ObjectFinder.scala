9dcd6329dd110726badcb4273dffb2288177edb3
CharafZellou <charaf.zellou@gmail.com>
2019-03-14 21:44:17 +0100

    Added some CSV parsing

--- src/main/scala/ObjectFinder.scala
+++ src/main/scala/ObjectFinder.scala
@@ -1,7 +1,15 @@
+import scala.collection.mutable.ArrayBuffer
+
 object ObjectFinder{
   def main(args: Array[String]): Unit = {
     val path = "C:/Users/chara/Downloads/CSVExemple.csv"
-    filterLinesFromFile(readLinesFromFile(path)).foreach(println)
+    val table = filterLinesFromFile(readLinesFromFile(path))
+    println(
+      for{
+        n <- table
+        na <- n
+      } yield (na).toString
+    )
     //lines.foreach(println)
   }
 
@@ -10,11 +18,18 @@ object ObjectFinder{
     Source.fromFile(path).getLines.toArray
   }
 
-  def filterLinesFromFile(array: Array[String]): Array[String] = {
+  def filterLinesFromFile(array: Array[String]): Array[Array[String]] = {
+    val rows = ArrayBuffer[Array[String]]()
+    for(line <- array){
+      rows += line.split(",").map(_.trim)
+    }
+    rows.toArray[Array[String]]
+    /*
     var newArray = new Array[String](0)
     for(line <- array){
       newArray = line.split(",").map(_.trim)
     }
     newArray
+    */
   }
 }
