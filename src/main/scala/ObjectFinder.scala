88933636bdca788ee6a10c43a0751bc8a7ba38c5
Crunchy <juanjur@hotmail.Es>
2019-03-19 00:12:52 +0100

    added cat recognition
    also cleaned some useless lines

--- src/main/scala/ObjectFinder.scala
+++ src/main/scala/ObjectFinder.scala
@@ -8,20 +8,11 @@ import scala.io._
 object ObjectFinder{
   def main(args: Array[String]): Unit = {
     //val path = "C:/Users/chara/Downloads/CSVExemple.csv"
-    val path: String = "D:/juanj/Downloads/CSVExemple.csv"
-    val table: Array[Array[String]] = filterLinesFromFile(readLinesFromFile(path))
+    val path = "D:/juanj/Downloads/CSVExemple.csv"
+    val table = filterLinesFromFile(readLinesFromFile(path))
+    val listOfObject = table.map(line => createObjectFromArray(line) )
 
-
-    //val objectTable: Array[Array[Object]] = filterSemiColumnFromFile(table)
-    /*println(
-      for{
-        n <- table
-        na <- n
-      } yield (na).toString
-    )*/
-    //lines.foreach(println)
-
-    table.foreach( line => line.foreach(println) )
+    listOfObject.foreach( line => println(line))
   }
 
   /*
@@ -35,32 +26,29 @@ object ObjectFinder{
   filterLinesFromFile split array by character "," and trim
    */
   def filterLinesFromFile(array: Array[String]): Array[Array[String]] = {
-    val result = array.map(line => line.split(",").map(_.trim) )
-    result
+    array.map(line => line.split(",").map(_.trim) )
   }
 
   /*
   semiColumnToSeq split array by ";" , trim it and return Seq[string]
    */
   def semiColumnToSeq(string: String) : Seq[String] = {
-    val splitted = string.split(";").map(_.trim)
-    splitted.toSeq
+    string.split(";").map(_.trim).toSeq
   }
 
-  /*
-  function not used but keeped in case of who knows
-   */
-  def filterSemiColumnFromFile(array: Array[Array[String]]): Array[Array[Object]] = {
-    val result = array.map( line => line.map( elem => if ( elem.contains(";") ) semiColumnToSeq(elem) else elem ) )
-    result
+  def stringIsInt( string: String ): Either[Exception, Int] = {
+    try Right(string.toInt)
+    catch { case e: Exception => Left(e) }
   }
 
   /*
   * try to fit Array to an object in classIndex
   * */
-  def fromArrayToObject( array: Array[String] ): Any = array match{
-    case headIsSeq if headIsSeq.head.contains(";") => Film(semiColumnToSeq(headIsSeq.head), new Date (headIsSeq(2)) )
-    case secondIsSeq if secondIsSeq(2).contains(";") => Actor( secondIsSeq.head, semiColumnToSeq(secondIsSeq(2)) )
-    case _ => Person("not implemented", "yet", 5000, 2)
+  def createObjectFromArray(array: Array[String] ): Any = array match{
+    case noResult if noResult.length < 2 => "this class is not implemented yet"
+    case film if film.head.contains(";") => Film( semiColumnToSeq(film.head), new Date ( film(1) ) )
+    case actor if actor(1).contains(";") => Actor( actor.head, semiColumnToSeq( actor(1) ) )
+    case cat if stringIsInt( cat(2) ).isRight && cat.length == 3 => Cat( cat.head, cat(1), cat(2).toInt )
+    case _ => "this class is not implemented yet"
   }
 }
